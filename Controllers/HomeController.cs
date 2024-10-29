36f21326364adfdc7fb177d8ad7e8041d5aee661
Wendrick Figueiredo <115407077+Wendrick-Figueiredo@users.noreply.github.com>
2024-10-29 10:15:24 -0300

    Teste

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult login()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
--- obj/Debug/net8.0/Projeto-Fatec.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto-Fatec.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto-Fatec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6e48c74a38eea68546f6b001a3c4a167e8fe7c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37da1a7747a8ee7a8d06dfae4b5612962b3a9a69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto-Fatec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto-Fatec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
